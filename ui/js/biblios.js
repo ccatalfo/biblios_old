09265567f28338edc7cb0664f24089af970c0aa5
Chris Catalfo <chris.catalfo@liblime.com>
2007-11-26 12:32:29 -0800

    Initial import of extjs2 version of biblios

---
+++ ui/js/biblios.js
@@ -0,0 +1,104 @@
+/**
+  * Application Layout
+  * by Jozef Sakalos, aka Saki
+  * http://extjs.com/learn/Tutorial:Application_Layout_for_Beginners
+  */
+ 
+// reference local blank image
+Ext.BLANK_IMAGE_URL = '../extjs/resources/images/default/s.gif';
+ 
+// create namespace
+Ext.namespace('biblios');
+ 
+// create application
+biblios.app = function() {
+    // do NOT access DOM from here; elements don't exist yet
+ 
+    // private variables
+ 
+    // private functions
+ 
+    // public space
+    return {
+        // public properties, e.g. strings to translate
+ 
+        // public methods
+        init: function() {
+            var viewport = new Ext.Viewport({
+				layout: 'border',
+				items: [
+					{
+						region: 'north',
+						layout: 'border',
+						items: [
+							{
+								region: 'center'
+							},
+							{
+								region: 'west'
+							},
+							{
+								region: 'east'
+							}
+						]
+					},
+					new Ext.TabPanel({
+						region: 'center',
+						items: [
+							{
+								title: 'Home',
+								closable: false,
+								autoScroll: true
+							},
+							{
+								title: 'Biblio',
+								closable: false,
+								autoScroll: true,
+								layout: 'border',
+								items: [
+									{
+										region: 'center',
+										layout: 'border',
+										items: [
+											{
+												region: 'center',
+												id: 'searchgrid'
+											},
+											{
+												region: 'center',
+												id: 'editor'
+											}
+										]
+									},
+									{
+										region: 'west',
+										split: true,
+										collapsible: true,
+										collapsed: true,
+										width: 200,
+										maxSize: 200,
+										title: 'Resources'
+									},
+									{
+										region: 'east',
+										split: true,
+										collapsible: true,
+										collapsed: true,
+										width: 200,
+										maxSize: 200,
+										title: 'Help'
+
+									}
+								]
+							}
+						]
+					})
+				]
+			});
+			
+		alert('Application successfully initialized');
+        }
+    };
+}(); // end of app
+ 
+// end of file
