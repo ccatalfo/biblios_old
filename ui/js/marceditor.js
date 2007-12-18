3bcdd54b52e82c79ae179f16c31d3cddd05e4962
Chris Catalfo <chris.catalfo@liblime.com>
2007-12-18 08:55:42 -0800

    Fixed bugs in marceditor relating up adding/deleting fields and not having underlying marcrecord instance updated.

--- ui/js/marceditor.js
+++ ui/js/marceditor.js
@@ -20,10 +20,14 @@ function MarcEditor(ffeditor, vareditor) {
 	}
 
 	function createFields() {
+		// make sure we start w/ empty arrays
+		fields.length = 0;
+		fieldlist.length = 0;
 		var editorfields = $('.tag');
 		for( var i = 0; i < editorfields.length; i++) {
 				var newfield = createField( editorfields.eq(i) );
 				fields.push(newfield);
+				fieldlist.push($(this).get(0).id.substring(0,3));
 		}
 		marcrecord = new MarcRecord(fields);
 	}
@@ -50,6 +54,29 @@ function MarcEditor(ffeditor, vareditor) {
 			}
 		}
 		return newfield;
+	}
+
+	function update(elem) {
+		// if we're passed a single element, just update that one
+		if( elem ) {
+			// get the tagnumber to update var tagnumber = $(elem).parents('.tag').children('.tagnumber').val();
+			// replace with this tag
+			var tag = $(elem).parents('.tag');
+			var tagnumber = $(elem).parents('.tag').find('.tagnumber').val();
+			// loop through fields in this marceditor instance, replacing the one whose tagnumber
+			// we're modifying
+			for( var i = 0; i < fields.length; i++) {
+				if( fields[i].tagnumber() == tagnumber ) {
+					fields[i] = createField(tag); 		
+				}
+			}
+		}
+		// otherwise update them all
+		else {
+			createFields();
+		}
+		//update marcrecord instance
+		marcrecord = new MarcRecord(fields);
 	}
 
 	// privileged methods
@@ -94,6 +121,7 @@ function MarcEditor(ffeditor, vareditor) {
 
 	this._setValue = function(tag, subfield, value) {
 		$('[@id^='+tag+']').children('.subfields').children('[@id*='+subfield+']').find('.subfield-text').val(value);
+		update();
 	}
 
 	this._getValue = function(tag, subfield) {
@@ -127,12 +155,85 @@ function MarcEditor(ffeditor, vareditor) {
 		}
 	}
 
-	this._addField = function(tag, ind1, ind2, subfields) {
-		addField(tag, ind1, ind2, subfields);
+	this._addField = function(tagnumber, ind1, ind2, subfields) {
+		if( tagnumber ) {
+			doAddField(tagnumber, ind1, ind2, subfields);
+		}
+		else {
+			Ext.MessageBox.prompt('Add tag', 'Choose a tag number', function(btn, tagnumber) {
+				doAddField(tagnumber);
+			});
+		}
+		function doAddField(tagnumber, ind1, ind2, subfields) {
+			var firstind = ind1 || '#';
+			var secondind = ind2 || '#';
+			var sf = subfields || [ {'delimiter': 'a', 'text': ''} ];
+			if(debug) { console.info("Adding tag with tagnumber: " + tagnumber);  }; 
+
+			// insert the new field in numerical order among the existing tags
+			var tags = $(".tag", UI.editor.doc );
+			var tagToInsertAfter; // the tag just before where we'll insert the new tag
+			var highestSuffix = 1; // highest number appended to tag id's with this tag number.  Add 1 to it to get suffix for new tag
+			var newSuffix = 1;
+			for( var i = 0; i<tags.length; i++) {
+				var id = $(tags[i]).attr('id').substr(0,3);
+				if( id < tagnumber) {
+					tagToInsertAfter = tags[i];
+				}
+				// get a new suffix number for our new tags id
+				else if( id == tagnumber ) {
+					var currSuffix = $(tags[i]).attr('id').substr(3);
+					if( currSuffix > highestSuffix ) {
+						highestSuffix = currSuffix;
+					} 
+				}
+			}
+			newSuffix = highestSuffix + 1;
+			var newId = tagnumber + "-" + newSuffix;
+			  var newtag = '<div class="tag" id="'+newId+'">';
+			  newtag += '<input class="tagnumber" id="d'+tagnumber+'" value="'+tagnumber+'" />';
+			  newtag += '<input size="2" class="indicator" value="'+firstind+'" id="dind1'+newId+'"/>';
+			  newtag += '<input size="2" class="indicator" value="'+secondind+'" id="dind2'+newId+'"/>';
+			  newtag += '<span class="subfields" id="dsubfields'+newId+'">';
+			  for( var i = 0; i< sf.length; i++) {
+				  newtag += '<span class="subfield" id="'+tagnumber+newId+'">';
+				  newtag += '<input class="subfield-delimiter" maxlength="2" size="2" value="&Dagger;'+sf[i]['delimiter']+'">';
+				  var textlength = sf[i]['text'].length;
+				  newtag += '<input id="'+tagnumber+newId+i+'text"class="subfield-text" size="'+textlength+'" value="'+sf[i]['text']+'">';
+				}
+			// insert out new tag after the tag we just found
+			$(tagToInsertAfter, UI.editor.doc).after(newtag);
+			update();
+			// set the focus to this new tag
+			//$( newId ).get(0).focus();
+		}
 	}
 
 	this._deleteField = function(tagnumber, i) {
-		removeTag(tagnumber, i);
+		if( tagnumber ) {
+			// remove  the ith tagnumber if we were passed an i
+			if( !Ext.isEmpty(i) ) {
+				$('.tag').filter('[@id*='+tagnumber+']').eq(i).remove();
+			}
+			// else remove all!
+			else {
+				$('.tag').filter('[@id*='+tagnumber+']').remove();
+			}
+			update();
+		}
+		else {
+			// focus previous or next tag
+			var prev = $(UI.editor.lastFocusedEl).parents('.tag').prev().children('.tagnumber');
+			var next = $(UI.editor.lastFocusedEl).parents('.tag').next().children('.tagnumber');
+			$(UI.editor.lastFocusedEl).parents('.tag').remove();
+			update();
+			if( $(next).length ) {
+				$(next).focus();
+			}
+			else {
+				$(prev).focus();
+			}
+		}
 	}
 
 	this._addSubfield = function(tag, subfield, value) {
@@ -148,6 +249,7 @@ function MarcEditor(ffeditor, vareditor) {
 			// set its value to pass in param
 			$(lastsf).parents('.subfield').next().children('.subfield-text').val(value);
 		}
+		update();
 		// focus the new subfield-text
 		$(lastsf).parents('.subfield').next().children('.subfield-text').focus();
 	}
@@ -156,10 +258,12 @@ function MarcEditor(ffeditor, vareditor) {
 		var sf = $('[@id^='+tag+']').children('.subfields').children('[@id*='+subfield+']').children('.subfield-text');
 		UI.editor.lastFocusedEl = sf;
 		removeSubfield();
+		update();
 	}
 
 	this._deleteSubfields = function(tag) {
 		$('[@id^='+tag+']').children('.subfields').find('.subfield').remove();
+		update();
 	}
 
 	this._focusTag = function(tag) {
@@ -179,19 +283,7 @@ function MarcEditor(ffeditor, vareditor) {
 	}
 
 	this._update = function(elem) {
-		// get the tagnumber to update
-		var tagnumber = $(elem).parents('.tag').children('.tagnumber').val();
-		// replace with this tag
-		var tag = $(elem).parents('.tag');
-		// loop through fields in this marceditor instance, replacing the one whose tagnumber
-		// we're modifying
-		for( var i = 0; i < fields.length; i++) {
-			if( fields[i].tagnumber() == tagnumber ) {
-				fields[i] = createField(tag); 		
-			}
-		}
-		//update marcrecord instance
-		marcrecord = new MarcRecord(fields);
+		update(elem);
 	}
 }
 // Public methods 
