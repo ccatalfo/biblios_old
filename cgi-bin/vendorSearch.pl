313bbd0d55097f10a4d1498fa36ea6567dc60421
Chris Catalfo <chris.catalfo@liblime.com>
2007-12-19 10:44:59 -0800

    Fixed couple of bugs in vendorSearch script.
    Authors weren't being concatenated from list to be included in xml element.  Turned off file::cache use of /tmp b/c as www-data couldn't seem to get correct permissions.

--- cgi-bin/vendorSearch.pl
+++ cgi-bin/vendorSearch.pl
@@ -8,9 +8,9 @@ use Cache::File;
 
 my $cgi = new CGI();
 my $cache = Cache::File->new(
-	cache_root => '/tmp/awscache',
+	cache_root => '/tmp',
 	default_expires => '30 min',
-);
+) or die "Couldn't write to cache temp file";
 
 my $xmlresponse = '';
 my $xmlwriter = new XML::Writer(OUTPUT => \$xmlresponse, NEWLINES=>0);
@@ -20,6 +20,7 @@ my $searchtype = $cgi->param('searchtype');
 my $query = $cgi->param('query');
 my @vendors = split(' ', $cgi->param('vendors'));
 
+print $cgi->header( -type => 'text/xml' );
 foreach my $vendor (@vendors) {
 	if( $vendor eq 'Amazon') {
 		# figure out correct power param based on query passed in
@@ -43,15 +44,16 @@ foreach my $vendor (@vendors) {
 		my $ua = Net::Amazon->new(
 			token => '14RFHHPWH7B3BHDSTDR2',
 			max_pages => 2,
-			cache => $cache
+			#cache => $cache
 		);
 		my $resp = $ua->search(power => "$powerquer:$query", mode => 'books');
-		print $cgi->header( -type => 'text/xml' );
 		if( $resp->is_success()) {
 			$xmlwriter->startTag('vendor', 'name'=>'Amazon');
+			$xmlwriter->dataElement('count', $resp->total_results() );
 			for( $resp->properties() ) {
+				$xmlwriter->startTag('item');
 				$xmlwriter->dataElement('title', $_->title() );
-				$xmlwriter->dataElement('author', $_->authors() );
+				$xmlwriter->dataElement('authors', join( ' ', $_->authors()) );
 				$xmlwriter->dataElement('publisher', $_->publisher() );
 				$xmlwriter->dataElement('pubdate', $_->publication_date() );
 				$xmlwriter->dataElement('release_date', $_->ReleaseDate() );
@@ -60,6 +62,7 @@ foreach my $vendor (@vendors) {
 				$xmlwriter->dataElement('description', $_->ProductDescription() );
 				$xmlwriter->dataElement('total_offers', $_->TotalOffers() );
 				$xmlwriter->dataElement('asin', $_->Asin() );
+				$xmlwriter->endTag('item');
 			}
 			$xmlwriter->endTag('vendor');
 		}
