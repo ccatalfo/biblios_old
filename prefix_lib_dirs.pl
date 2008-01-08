8891fc4df048718794258d4522d74d6c6f3be63b
Chris Catalfo <chris.catalfo@liblime.com>
2008-01-08 08:17:56 -0800

    Small perl script to update javascript import dirs with a given prefix.
    Run prefix_lib_dirs.pl index.html index-koha.html "/koha-tmpl/intranet-tmpl/prog/en/lib/biblios/" to update all lib imports in index.html and output to index-koha.html.

---
+++ prefix_lib_dirs.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $infile = shift(@ARGV);
+my $outfile = shift(@ARGV);
+my $prefix = shift(@ARGV);
+open(IN, $infile) or die "can't open $infile for reading $!";
+open(OUT, ">$outfile") or die "can't open $outfile for writing $!";
+
+while(<IN>) {
+	if( $_ =~ /.*src="(.*)".*/ ) {
+		print 'Updating ' . $_ . 'to ' . $prefix.$1 . "\n";
+		my $newdir = $prefix . $1;
+		$_ =~ s/$1/$newdir/;
+	}
+	print OUT $_;
+}
