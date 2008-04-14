ac7811cf58ceaccae89f53a125c77b1f3b51effa
Chris Catalfo <chris.catalfo@liblime.com>
2008-04-14 12:10:30 -0700

    Adding more loading updates.

--- ui/js/biblios.js
+++ ui/js/biblios.js
@@ -27,6 +27,7 @@ biblios.app = function() {
     var paz = {};
 	
 	var savefileSelectSql = 'SELECT Records.rowid as Id, Records.title as Title, Records.author as Author, Records.date as DateOfPub, Records.location as Location, Records.publisher as Publisher, Records.medium as Medium, Records.xml as xml, Records.status as Status, Records.date_added as DateAdded, Records.date_modified as DateModified, Records.xmlformat as xmlformat, Records.marcflavour as marcflavour, Records.template as template, Records.marcformat as marcformat, Records.Savefiles_id as Savefiles_id, Records.SearchTargets_id as SearchTargets_id FROM Records';
+    Ext.get('loadingtext').update('Reading biblios configuration file');
     Ext.Ajax.request({
 		url: confPath,
 		method: 'GET',
