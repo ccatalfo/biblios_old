e615b5aaa61c3d7c037b39379328d63c9839650c
Chris Catalfo <chris.catalfo@liblime.com>
2008-05-06 10:06:59 -0700

    Allow uploading of marcxml files.

--- cgi-bin/uploadMarc.pl
+++ cgi-bin/uploadMarc.pl
@@ -7,6 +7,7 @@ use MARC::Record;
 use MARC::Batch;
 use MARC::File::XML;
 use File::Basename;
+use File::Slurp qw(slurp);
 use File::Temp qw(tempfile);
 use JSON;
 
@@ -17,24 +18,32 @@ my $records = '';
 
 my $filepath = $cgi->param('file');
 my $fh = $cgi->upload("file");
-my $filename = fileparse($filepath);
-#warn "uploadMarc.pl got filename: " . $filename;
+my ($filename, $directories, $suffix) = fileparse($filepath, , qr/\.[^.]*/);
+#warn "uploadMarc.pl got filename: " . $filename . " with suffix: " . $suffix;
 
 print $cgi->header( -type => 'text/html' );
 
-my $batch = MARC::Batch->new('USMARC', $fh) or warn "can't open $filename in marc::batch";
-$records .= MARC::File::XML::header();
-#warn MARC::File::XML::header();
-while(my $record = $batch->next() ){
-  warn "got record with title: " . $record->title();
-  $records .= MARC::File::XML::record($record);
-  #warn "xml: " . MARC::File::XML::record($record);
+if( $suffix eq '.mrc' ) {
+    my $batch = MARC::Batch->new('USMARC', $fh) or warn "can't open $filename in marc::batch";
+    $records .= MARC::File::XML::header();
+    #warn MARC::File::XML::header();
+    while(my $record = $batch->next() ){
+      #warn "got record with title: " . $record->title();
+      $records .= MARC::File::XML::record($record);
+      #warn "xml: " . MARC::File::XML::record($record);
+    $records .= MARC::File::XML::footer();
+    }
 }
-$records .= MARC::File::XML::footer();
+elsif ( $suffix eq '.xml' ) {
+    $records = slurp($fh) or die "can't open $filename for reading";
+}
 #warn MARC::File::XML::footer();
+else {
+    $response->{success} = "false";
+}
 
 print $returnfh $records;
-close$returnfh;
+close $returnfh;
 $response->{success} = "true";
 $response->{filepath} = fileparse($returnfilepath);
 #warn to_json($response);
