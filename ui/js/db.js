4d681723876e35e6ffccab02324f2a5af7ed88cf
Chris Catalfo <chris.catalfo@liblime.com>
2008-05-09 12:43:45 -0700

    Add initcall field to plugins table.

--- ui/js/db.js
+++ ui/js/db.js
@@ -393,6 +393,7 @@ function init_gears() {
                     name: new GearsORM.Fields.String(),
                     file: new GearsORM.Fields.String(),
                     type: new GearsORM.Fields.String(),
+                    initcall: new GearsORM.Fields.String(),
                     enabled: new GearsORM.Fields.Integer({defaultValue:0}),
                     allowDelete: new GearsORM.Fields.Integer({defaultValue:1}),
                     allowModify: new GearsORM.Fields.Integer({defaultValue:1})
