15bac9fbe3ecb1ea268d9c9923a9c362fb9c4ffc
Chris Catalfo <chris.catalfo@liblime.com>
2008-06-11 08:31:53 -0700

    Patch in few remaining methods for marcviewer example plugin.

--- plugins/marcviewer.js
+++ plugins/marcviewer.js
@@ -1,16 +1,36 @@
 MarcViewer = function(editorelem, editorid) {
     var editorid = editorid;
     var editorelem = editorelem;
+    var xmldoc = null;
+    
+    this._XMLString = function() {
+        return (new XMLSerializer().serializeToString( xmldoc ));
+    };
 
+    this._loadXml = function(marcXmlDoc) {
+        xmldoc = marcXmlDoc;
+        var processor = new XSLTProcessor();
+        processor.importStylesheet( marcxsl );
+        var editorDoc = processor.transformToDocument( marcXmlDoc );
+        return (new XMLSerializer().serializeToString( editorDoc ));
+    };
 }
+
 MarcViewer.prototype.loadXml = function( marcXmlDoc ) {
-    var processor = new XSLTProcessor();
-    processor.importStylesheet( marcxsl );
-    var editorDoc = processor.transformToDocument( marcXmlDoc );
-    return (new XMLSerializer().serializeToString( editorDoc ));
+    return this._loadXml(marcXmlDoc);
 }
+
 
 MarcViewer.prototype.postProcess = function( ) {
     return true;
 }
 
+MarcViewer.prototype.update = function() {
+    return true;
+}
+MarcViewer.prototype.XMLString = function() {
+    return this._XMLString(); 
+}
+MarcViewer.prototype.getToolsMenu = function() {
+    return new Array();
+}
