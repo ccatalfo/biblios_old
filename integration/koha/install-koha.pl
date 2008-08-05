69b01eece0e8d76374d9ef5541c5deca4322ff40
Chris Catalfo <chris.catalfo@liblime.com>
2008-08-05 12:10:31 -0700

    Add back in simple perl script to install Biblios into Koha dir tree.
    integration/koha/install-koha.pl prompts user for Koha installation parameters (using term::clui), runs src/index.html through Template Toolkit processor and then copies Biblios src files into appropriate Koha paths.  Removed koha make target since it is no longer necessary.  Updated INSTALL.koha to reflect new installation procedure.

---
+++ integration/koha/install-koha.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Template;
+use Term::Clui;
+
+my $kohadir = ask("What is the base directory of your koha install?");
+my $langtheme = ask("What is the language/theme path for koha (e.g.) /prog/en", "/prog/en");
+my $kohastaffport = ask("What port is your koha staff intranet running on?", "80");
+my $kohaurl = ask("What is the url of your staff intranet (without port and without trailing slash)?", "");
+
+
+my $tt = Template->new({
+  PRE_PROCESS => 'conf/build-koha.conf',
+  RELATIVE => 1,
+}) || die "$Template::ERROR\n";
+
+print "Building Biblios for installation into Koha\n";
+my $vars = {
+  hostPort => $kohastaffport,
+  kohaurl => $kohaurl,
+  embeddedUrl => $kohaurl . ":" . $kohastaffport . "/"
+};
+$tt->process('./src/index.html', $vars, './build/index.html')
+  || die $tt->error(), "\n";
+
+#my $buildstatus = `make koha`;
+
+print "Installing biblios into $kohadir \nwith language/theme path $langtheme \nsetting up biblios to use port $kohastaffport for koha's intranet site\n";
+my $status = `make koha-install KOHADIR=$kohadir KOHALANGTHEME=$langtheme KOHASTAFFPORT=$kohastaffport`
