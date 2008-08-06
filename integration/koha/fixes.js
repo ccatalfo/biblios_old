a7d8221a20d29eeafd7094f6b94fa50f615f1a8d
Chris Catalfo <chris.catalfo@liblime.com>
2008-08-06 10:11:27 -0700

    Removing obsoleted koha integration files: doc-head-close-biblios.inc, fixes.js, updateForEmbedding.pl.

--- integration/koha/fixes.js
+++
@@ -1,9 +0,0 @@
-	// fixes for embedding into koha (eliminate css clashes with koha css)
-	// borders on unclassed td
-	$('td').css('border-left', 'none').css('border-top', 'none').css('border-bottom', 'none').css('background-color', 'transparent');
-	// borders on tables
-	$('table').css('border-collapse', 'separate').css('border-right', 'none').css('border-top', 'none');
-	// prevent scrollbars from overlapping grid (noted in mac ff and windows ff)
-	$('.x-grid-scroller').css('overflow', 'hidden');
-	// make "More" koha dropdown appear correctly
-	$('.outerbox.inner').css('position', '');
