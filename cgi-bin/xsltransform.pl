c246d1a9e7b47f0d6c640ea0f19c48b821296c9e
Chris Catalfo <fuzzy@local-debian.localnet>
2008-09-26 21:22:13 -0400

    Add perl cgi xsltransform.pl script for performing xslt transformations server side.

---
+++ cgi-bin/xsltransform.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use XML::LibXSLT;
+use XML::LibXML;
+use CGI;
+use CGI::Carp;
+use XML::Simple;
+use JSON;
+use Data::Dumper;
+require Encode;
+
+my $debug = 1;
+my $parser = XML::LibXML->new();
+my $xslt = XML::LibXSLT->new();
+my $cgi = CGI->new();
+
+my $xsl_path = $cgi->param('xslpath');
+if($debug) {warn "Using $xsl_path as biblios xsl path";}
+
+my $xml = Encode::decode_utf8( $cgi->param('xml') );
+my $xsl_file = $cgi->param('stylesheet');
+my $editorid = $cgi->param('editorid') || 'editorone';
+my $contenttype = $cgi->param('contenttype') || 'text/html';
+if($debug){
+	warn "xsltransform: generating for editorid: $editorid";
+}
+
+my $source = $parser->parse_string($xml);
+my $style_doc = $parser->parse_file($xsl_path . $xsl_file) or die "Can't open stylesheet $xsl_file";
+my $stylesheet = $xslt->parse_stylesheet($style_doc) or die "Can't parse stylesheet $!";
+my $results = $stylesheet->transform($source, XML::LibXSLT::xpath_to_string(editorid => $editorid)) or die "Can't transform $!";
+
+print $cgi->header(-type=>$contenttype, -charset=>'utf-8');
+# output_as_chars should return utf-8
+print $stylesheet->output_string($results);
+
+
