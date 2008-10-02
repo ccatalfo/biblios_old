59374e8644b4b1d436edc7362a7ff0e758ef5208
Chris Catalfo <cfc@biblios.ec2.liblime.com>
2008-10-02 13:13:22 -0400

    Remove uiPath from path for new trash icon.  Also make new trash icon default for new gears db's.

--- ui/js/db.js
+++ ui/js/db.js
@@ -78,7 +78,7 @@ GearsORMShift.rules = [
 				allowDrag: 0,
 				allowDrop: 0,
 				ddGroup: null,
-				icon: libPath + 'ui/images/user-trash.png',
+				icon: uiPath + 'ui/images/toolbar/trash.gif',
 				date_added: 'datetime("now", "localtime"',
 				date_mofified: 'datetime("now", "localtime"'
 			}).save();
@@ -288,7 +288,7 @@ GearsORMShift.rules = [
         comment: 'Change trash icon in Trash Save folder to new icon',
         up: function() {
             var trash = DB.Savefiles.select(1).getOne();
-            trash.icon = uiPath + 'ui/images/toolbar/trash.gif';
+            trash.icon = 'ui/images/toolbar/trash.gif';
             trash.save();
             return true;
         },
